bf2ae4183813403f22fb161eb99670cdb5ed1745
AJ Beck <aj@asteroid.engineering>
2023-03-03 10:36:59 -0600

    fix: Update bash to set multiline github action output

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,7 @@ ls -l -a
 PRETTIER_CHECK_OUTPUT="$(prettier --config /default-prettier-config.json --check .)"
 PRETTIER_CHECK_EXIT_CODE=$?
 
-echo "check_output=$PRETTIER_CHECK_OUTPUT" >> $GITHUB_OUTPUT
+echo 'check_output<<EOF' >> $GITHUB_OUTPUT
+echo "$PRETTIER_CHECK_OUTPUT" >> $GITHUB_OUTPUT
+echo 'EOF' >> $GITHUB_OUTPUT
 echo "check_exit_code=$PRETTIER_CHECK_EXIT_CODE" >> $GITHUB_OUTPUT
